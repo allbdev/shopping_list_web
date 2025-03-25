b59c04fc7f8cd295d207ddf8776aea07eca0f016
Albuquerque <alb.develloper@gmail.com>
2025-03-25 17:17:53 -0300

    feat: input component

---
+++ components/Input.tsx
@@ -0,0 +1,7 @@
+"use client";
+import React from "react";
+import { TextField } from "@radix-ui/themes";
+
+export const Input = (props: TextField.RootProps) => {
+  return <TextField.Root {...props} />;
+};
